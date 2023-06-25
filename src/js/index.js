cc8ddc0b0ea9bd356da3049b2ecc92b761ee529a
Alamin <codepapa360@gmail.com>
2023-06-25 14:26:38 +0600

    Finished major features

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,26 @@
 "use strict";
 import "../sass/main.scss";
+
+const dropdownParent = document.querySelector(".nav-links");
+const dropdownAll = dropdownParent.querySelectorAll(".dropdown");
+const hamberger = document.querySelector(".hamberger-menu");
+const nav = document.querySelector(".navigation");
+const overlay = document.querySelector("#overlay");
+
+dropdownParent.addEventListener("click", function (e) {
+  const drop = e.target.closest(".dropdown");
+  if (!drop) return;
+
+  dropdownAll.forEach((el) => {
+    if (el !== drop) el.classList.remove("expanded");
+  });
+
+  drop.classList.toggle("expanded");
+});
+
+[hamberger, overlay].forEach((el) => {
+  el.addEventListener("click", function () {
+    nav.classList.toggle("open");
+    overlay.classList.toggle("open");
+  });
+});
